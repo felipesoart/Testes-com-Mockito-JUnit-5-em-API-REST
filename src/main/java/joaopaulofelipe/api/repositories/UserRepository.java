7ba1e318e587a25bd61e260a0b63ffae650ce22e
felipesoart <felipesoart@gmail.com>
2025-03-26 16:01:36 -0300

    interfeca user resource

---
+++ src/main/java/joaopaulofelipe/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package joaopaulofelipe.api.repositories;
+
+import joaopaulofelipe.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
