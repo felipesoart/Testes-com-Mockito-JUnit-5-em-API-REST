aa8b0cae4482cd69fd8aa945d51a9c7023ff504a
felipesoart <felipesoart@gmail.com>
2025-03-31 14:59:34 -0300

    endpoint findAll para User

--- src/main/java/joaopaulofelipe/api/services/UserService.java
+++ src/main/java/joaopaulofelipe/api/services/UserService.java
@@ -2,6 +2,11 @@ package joaopaulofelipe.api.services;
 
 import joaopaulofelipe.api.domain.User;
 
+import java.util.List;
+
+
 public interface UserService {
     User findById(Integer id);
+
+    List<User> findAll();
 }
--- src/main/java/joaopaulofelipe/api/services/impl/UserServiceImpl.java
+++ src/main/java/joaopaulofelipe/api/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import joaopaulofelipe.api.services.exceptions.ObjctNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,10 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjctNotFoundException("Objeto não encontrado"));
+    }
+
+
+    public List<User> findAll(){
+        return repository.findAll();
     }
 }
